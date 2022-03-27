388eba27ade573b6a244b49cdf6d31ebf8808232
Jefferson Seno <jefferson.seno@datacake.com.br>
2022-03-27 15:12:40 -0300

    Adicionando códigos de achar elemento pela tag e pela classe dele

---
+++ findElementClass.py
@@ -0,0 +1,23 @@
+""" Importação da biblioteca do Selenium """
+from selenium import webdriver
+
+""" Instanciando o driver do navegador que irei usar """
+driver = webdriver.Chrome(executable_path=r"E:\Webdriver\chromedriver.exe")
+
+""" Abro a página que quero navegar """
+driver.get('https://statusinvest.com.br/fundos-imobiliarios/kfof11')
+
+""" Espera 3 segundos para o elemento aparecer na tela """
+driver.implicitly_wait(3) # Espera 3 segundos para o elemento aparecer na tela  
+
+""" Busca dos elementos dentro da página pelo nome da classe"""
+dados0 = driver.find_element_by_class_name('value').text # Busca de elemento pelo class_name
+dados1 = driver.find_elements_by_class_name('value')[0].text # Busca de elemento pelo class_name em uma lista
+dados2 = driver.find_elements_by_class_name('value')[3].text # Busca de elemento pelo class_name em uma lista
+dados3 = driver.find_elements_by_class_name('value')[4].text #
+
+# Mostra os dados
+print(dados0)
+print(dados1)
+print(dados2)
+print(dados3)
---
+++ findElementTagName.py
@@ -0,0 +1,24 @@
+""" Importando o módulo webdriver do Selenium """
+from selenium import webdriver
+
+""" Instanciando o navegador que irei usar """
+driver = webdriver.Chrome(executable_path=r"E:\Webdriver\chromedriver.exe")
+
+""" Abro a página que quero navegar """
+driver.get("https://statusinvest.com.br/fundos-imobiliarios/kfof11")
+
+""" Espera 3 segundos para o elemento aparecer na tela """
+driver.implicitly_wait(3) # Espera 3 segundos para o elemento aparecer na tela
+
+nomeFii = driver.find_elements_by_tag_name('h1')[0].text # Busca de elemento pelo tag_name
+
+valorAtual = driver.find_elements_by_tag_name('strong')[0].text # Busca de elemento pelo tag_name em uma lista
+
+tabelaRendimentos = driver.find_elements_by_tag_name('table')[0].text # Busca de elemento pelo tag_name em uma lista
+
+tabelaResultados = driver.find_elements_by_tag_name('table')[1].text # Busca de elemento pelo tag_name em uma lista
+
+print(nomeFii)
+print(valorAtual)
+print(tabelaRendimentos)
+print(tabelaResultados)
